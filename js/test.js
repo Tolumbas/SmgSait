f08f5e36911a24b600edef53b47b84e60f0683a3
Feiro <tzat.anton@gmail.com>
2017-03-31 20:00:55 +0300

    made responcive menu

--- js/test.js
+++ js/test.js
@@ -3,8 +3,26 @@
 
 $("#menu").stick_in_parent();
 
-$(".menubutton").hover(function (e){
-	$(this).children(".submenu").addClass("show");
-},function(e){
-	$(this).children(".submenu").removeClass("show");
+$(window).click(e=>{
+	$(".submenu").removeClass("show");
+	$("#rightmenu").removeClass("shown");
+	$("#hamburger").addClass("shown");
+})
+
+$(".menubutton").click(function (e){
+	$(".submenu")
+	.not(
+		$(this).children(".submenu")
+	)
+	.removeClass("show");
+	$(this).children(".submenu").toggleClass("show");
+	e.stopPropagation();
 });
+
+$("#hamburger").click(e=>{
+	$("#rightmenu").toggleClass("shown");
+	$("#hamburger").toggleClass("shown");
+	e.stopPropagation();
+
+})
+$(".menubutton").has(".submenu").children("a").addClass("disabled");
